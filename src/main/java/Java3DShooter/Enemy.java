b806f0d9574b80d520e63f34ed5838a5caaf85e7
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-09-24 14:21:56 -0500

    I think I got enemy spawns set up. It's some very difficult math using a uniform distribution that I had to work with AI to help me get (and understand) but I think I did a good job at commenting on the code to explain the math. I don't fully understand how it works but I think I understand the idea of it. I don't like peeling ideas directly from AI without an in-depth explanation which I feel I did a good job at providing it here

--- src/main/java/Java3DShooter/Enemy.java
+++ src/main/java/Java3DShooter/Enemy.java
@@ -3,6 +3,7 @@ package Java3DShooter;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import java.util.Random;
 
 public class Enemy extends Box {
 
@@ -27,18 +28,35 @@ public class Enemy extends Box {
     private static final double DEPTH = 10;
 
     /**
+     * Serves as the random number generator for the programmer, used to determine spawn locations. Seed is set upon class definition
+     */
+    private static final Random rand = new Random(System.currentTimeMillis());
+
+    /**
      * Color of the enemy's sprite
      */
     private static final Color COLOR = Color.RED;
 
     /**
+     * Speed of the enemy
+     */
+    private static final double SPEED = 1;
+
+    /**
+     * The closest an enemy can spawn to the player
+     */
+    private static int minDistanceFromPlayerOnSpawn = 500;
+
+    /**
      * initializes a new Enemy
      * <p>
      * Ensure that you call {@link #setGroundPlaneBoundingBox(double[][])} beforehand so the enemy's spawn coordinates can be determined
+     * @param player Box of the player
      * @throws IllegalArgumentException if the {@link #setGroundPlaneBoundingBox(double[][])} was not called beforehand
      */
-    public Enemy() throws IllegalArgumentException {
+    public Enemy(Box player) throws IllegalArgumentException {
         super(WIDTH, HEIGHT, DEPTH);
+        super.setTranslateY(groundPlaneBoundingBox[1][0] - HEIGHT/2);  // Y position doesn't change for enemies so we assign it here and never change it
 
         // Not the best practice way to do things, but it ensures we have fast creation of enemies
         // Better than passing the groundPlane every time we create an enemy
@@ -48,6 +66,55 @@ public class Enemy extends Box {
 
         PhongMaterial material = new PhongMaterial(COLOR);
         super.setMaterial(material);
+
+        this.setSpawn(player);
+    }
+
+    /**
+     * Sets the spawn location for the enemy based
+     */
+    private void setSpawn(Box player) {
+        // Credit where it's due, this was not my original idea. I got this from AI and then prompted it several times till I (somewhat) understood the math behind it.
+        // I believe AI should be used as a learning tool and not to do everything for me
+        // Hopefully my comments do a good job at showing my understanding of the topic rather than blind trust in a random equation an artificial intellegence suggested
+        // That being said this is quite ingenius and I'm sad I didn't think of something like this myself, but I know myself well enough to know I couldn't do something like this without seeing it first
+
+        // Get the player's x and z coords
+        double px = player.getTranslateX();
+        double pz = player.getTranslateZ();
+
+        // Strap in gamers, this code gets funky
+        // I'm talking Austin Powers funky
+        // My explanation is pretty bad here, but it uses a uniform distribution which will allow it to have equal chance of spawning anywhere outside the no-spawn ring around the player
+        // It's a very complex formula but hopefully my explanation does a decent job at explaning this.
+
+
+        // Get a random angle to spawn the enemy relative to
+        double angle = rand.nextDouble() * 2 * Math.PI;
+
+        // The minimum distance for a radius^2 ring of our spawn circle
+        // We use r^2 since the area of a circle grows with it's radius^2
+        // If we didn't do this it wouldn't be uniform and the spawn ring would be more likely to be close to the minDistanceFromPlayerOnSpawn
+        double minR2 = Math.pow(minDistanceFromPlayerOnSpawn, 2);
+
+        // The maximum distance for a radius^2 ring of our spawn circle
+        // We use the length of the groundPlaneBoundingBox divided by two which gives us a circle that is inside of this plane
+        // It doesn't reach the farthest corners, but it is contained entirelly within the ground plane
+        // This all assumes the x and z coordinates of the ground plane form a square otherwise this will not work
+        double maxR2 = Math.pow((groundPlaneBoundingBox[0][1] - groundPlaneBoundingBox[0][0]) / 2, 2);
+
+        // Gets a random radius^2 within our possible radii for the spawn ring then sqrt's it so we can convert it back to Cartesian coordinates and then assign that position to the enemy
+        double radius = Math.sqrt(minR2 + rand.nextDouble() * (maxR2 - minR2));
+
+        // Convert from Polar coordinates to Cartesian for the enemy to spawn at
+        // Polar to Cartesian form = (h + r * cos(angle), k + r * sin(angle))
+        // Where (h, k) is the center of the circle (in our case the player's location is (h,k)
+        double x = px + radius * Math.cos(angle);
+        double z = pz + radius * Math.sin(angle);
+
+        // Then we set these new coordinates
+        this.setTranslateX(x);
+        this.setTranslateZ(z);
     }
 
     /**
@@ -56,11 +123,23 @@ public class Enemy extends Box {
      * Must be called before instantiation of an Enemy
      * @param boundingBox the bounding box as defined by {{xMin, xMax}, {yMin, yMax}, {zMin, zMax}}
      */
-    public static void setGroundPlaneBoundingBox(double[][] boundingBox) {groundPlaneBoundingBox = boundingBox;}
+    public static void setGroundPlaneBoundingBox(double[][] boundingBox) { groundPlaneBoundingBox = boundingBox; }
 
     /**
      * Returns the set groundPlaneBoundingBox
      * @return the bounding box as defined by {{xMin, xMax}, {yMin, yMax}, {zMin, zMax}}
      */
     public static double[][] getGroundPlaneBoundingBox() {return groundPlaneBoundingBox;}
+
+    public void move(double playerX, double playerZ) {
+        double dx = this.getTranslateX() - playerX;
+        double dz = this.getTranslateZ() - playerZ;
+
+        // Note that in this case: cos(a) = z; sin(a) = x
+        // Thus in a tan() x->y and z->x
+        double angle = Math.atan2(dx, dz);  // In radians so we don't need to convert it later
+
+        this.setTranslateX(this.getTranslateX() + (SPEED * Math.sin(angle)));
+        this.setTranslateZ(this.getTranslateZ() + (SPEED * Math.cos(angle)));
+    }
 }
